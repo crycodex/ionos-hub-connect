af15e1e9859061bfdec9a2afb03bab6111c2875f
crycodex <recaldecd@gmail.com>
2025-10-27 16:11:31 -0500

    delete cursor

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { Suspense, lazy } from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BlogRecursos from "./pages/BlogRecursos";
-import BlobCursor from "./components/BlobCursor";
 import ScrollToTop from "./components/ScrollToTop";
 import TerminosCondiciones from "./pages/TerminosCondiciones";
 
@@ -34,24 +33,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BlobCursor
-        blobType="circle"
-        fillColor="hsl(204, 100%, 37%)" 
-        trailCount={3}
-        sizes={[40, 80, 50]}
-        innerSizes={[20, 35, 25]}
-        innerColor="hsl(199, 100%, 45%)"
-        opacities={[0.6, 0.6, 0.6]}
-        shadowColor="hsl(210, 100%, 50%, 0.3)"
-        shadowBlur={5}
-        shadowOffsetX={10}
-        shadowOffsetY={10}
-        filterStdDeviation={30}
-        useFilter={true}
-        fastDuration={0.1}
-        slowDuration={0.5}
-        zIndex={100}
-      />
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
